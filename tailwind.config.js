9c35889cbba20434dfcb680c0a928722627eb94b
Tunay Özdemir <tunayozdemir@192.168.1.93>
2025-04-19 13:14:33 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E40AF",
+        secondary: {
+          100: "#E0E7FF",
+          200: "#C7D2FE",
+          300: "#A5B4FC",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
