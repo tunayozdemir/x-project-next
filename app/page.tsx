5d060af2e977fa77ce68d26d80d04b9742fd9039
Tunay Özdemir <tunayozdemir@Tunays-MacBook-Pro.local>
2025-04-22 17:25:06 +0300

    test yapısı

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 
 import { Header, Footer } from "../components";
+import TestPage from "./TestPage/page"
 
 export default function Home() {
   return (
     <div className="bg-red-400">
-      <Header/>
-      <Footer/>
+      <Header />
+      <TestPage />
+      <Footer />
     </div>
   );
 }
